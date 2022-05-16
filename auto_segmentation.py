c4f9ff2c0e3bc4df7a425208a0e05809ab880416
StartingPointM <ma_hanwei@163.com>
2022-05-16 19:24:39 +0800

    update

--- auto_segmentation.py
+++ auto_segmentation.py
@@ -5,8 +5,7 @@ import matplotlib.pyplot as plt
 import json
 import os
 import matplotlib.image as mpimg
-
-
+ss
 
 img_path = "C:\\Users\\MAHanwei\\PycharmProjects\\Defect-Dtection\\resource\\test\\res\\template_1.jpg"
 img_bgr = cv2.imread(img_path)
